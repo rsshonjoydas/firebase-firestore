827112fe890f71fb19125a2c8f4d599d4737309c
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-25 05:11:43 +0600

    deleting data

--- app.js
+++ app.js
@@ -6,15 +6,24 @@ function renderCafe(doc) {
   let li = document.createElement('li');
   let name = document.createElement('span');
   let city = document.createElement('span');
+  let cross = document.createElement('div')
 
   li.setAttribute('data-id', doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  cross.textContent = 'x'
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(cross);
 
   cafeList.appendChild(li);
+
+  // ? deleting data
+  cross.addEventListener('click', (e) => {
+    e.stopPropagation()
+    db.collection('cafes').doc(doc.id).delete()
+  })
 }
 
 db.collection('cafes')
